1cbb83c12410f072239dbf3ad86794a6063ed74c
Ilya Pashkovskiy <ilya.pashkovskiy@gmail.com>
2017-12-27 20:02:41 +0100

    refactorings

--- MyBot.py
+++ MyBot.py
@@ -4,26 +4,29 @@ import hlt
 import logging
 from collections import defaultdict
 
-targeted_planets = []
+targeted_entities = []
 targets = defaultdict(None)
 
 
-def find_nearest_entity(entity, ship, game_map):
-    planets = {k: v for k, v in game_map.nearby_entities_by_distance(
+def find_nearest_entity(entity, ship, game_map, filters=[]):
+    entities = {k: v for k, v in game_map.nearby_entities_by_distance(
         ship).items() if isinstance(v[0], entity)}
 
-    planets_sorted = sorted(zip(planets.keys(), planets.values()))
-    for p in planets_sorted:
-        planet = p[1][0]
-        logging.info(planet)
-        if planet.is_owned():
+    entities_sorted = sorted(zip(entities.keys(), entities.values()))
+    for e in entities_sorted:
+        entity = e[1][0]
+        logging.info(entity)
+        needs_skipping = False
+        for f in filters:
+            needs_skipping = needs_skipping or f(entity)
+        if needs_skipping:
             continue
-        return planet
+        return entity
     return None
 
 
 def find_nearest_planet(ship, game_map):
-    return find_nearest_entity(hlt.entity.Planet, ship, game_map)
+    return find_nearest_entity(hlt.entity.Planet, ship, game_map, [lambda p: p.is_owned()])
 
 
 def find_new_target(ship, game_map):
@@ -42,7 +45,7 @@ def navigate_to(target, ship, game_map):
         command_queue.append(navigate_command)
 
 
-game = hlt.Game("Settler-v2")
+game = hlt.Game("Settler-v3")
 logging.info("Starting my Settler bot!")
 
 while True:
