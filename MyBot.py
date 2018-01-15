bdca092167238617891d1cc73bcea37c0fd388f6
Ilya Pashkovskiy <ilya.pashkovskiy@gmail.com>
2018-01-15 06:13:09 +0100

    initial working bot based on flow-field

--- MyBot.py
+++ MyBot.py
@@ -2,9 +2,7 @@
 """
 import hlt
 import logging
-from starfighter import Starfighter
-from command_center import CommandCenter
-from collections import defaultdict
+from flow_field import FlowField
 
 game = hlt.Game("Starfighter-v1")
 logging.info("Starting my Starfighter bot!")
@@ -17,49 +15,14 @@ def cmd(ship, force):
     return ship.thrust(force.norm(), force.argument())
 
 
-fighters = defaultdict(Starfighter)
-cc = CommandCenter()
 while True:
     game_map = game.update_map()
-    cc.set_map(game_map)
+    field = FlowField(game_map)
 
     command_queue = []
     for ship in game_map.get_me().all_ships():
-        fighter = fighters[ship.id]
-        fighter.set_position(ship.x, ship.y)
-
-    for ship in game_map.get_me().all_ships():
-        if ship.docking_status != ship.DockingStatus.UNDOCKED:
-            # fighters.pop(ship.id)
-            continue
-
-        target = cc.select_target(ship)
-        fighter = fighters[ship.id]
-
-        obstacles = game_map.all_planets()
-        if isinstance(target.entity, hlt.entity.Planet):
-            obstacles.remove(target.entity)
-
-        if ship.can_dock(target.entity):
-            fighter.set_velocity(0, 0)
-            command_queue.append(ship.dock(target.entity))
-            continue
-
-        target_point = ship.closest_point_to(target.entity)
-        speed, angle = ship.navigate(
-            target_point,
-            game_map,
-            speed=int(hlt.constants.MAX_SPEED),
-            ignore_ships=False,
-            angular_step=3)
-        fighter.set_velocity(speed, angle)
-        fighter.set_target(target_point.x, target_point.y)
-        other_fighters_position = [fighters[k]
-                                   for k in fighters.keys() if k != ship.id]
-        fighter.update(other_fighters_position, obstacles)
-        command_queue.append(cmd(ship, fighter.velocity))
+        command_queue.append(cmd(ship, field.lookup(ship.x, ship.y)))
 
     game.send_command_queue(command_queue)
-    logging.info(fighters)
     # TURN END
 # GAME END
--- flow_field.py
+++ flow_field.py
@@ -1,14 +1,23 @@
 import hlt
+import math
+from hlt.constants import MAX_SPEED
 from vector import Vector
 
 
 class FlowField(object):
     def __init__(self, game_map):
         self.game_map = game_map
-        self.data = [[Vector(1, 0) for x in range(self.game_map.width)]
-                     for y in range(self.game_map.height)]
+        self.width = game_map.width
+        self.height = game_map.height
+        self.data = [[Vector(1, 0) for y in range(self.height)]
+                     for x in range(self.width)]
 
-    def lookup(self, position):
-        x = int(Vector(1, 0) * position)
-        y = int(Vector(0, 1) * position)
-        return self.data[x][y]
+    def lookup(self, x, y):
+        x = math.floor(x)
+        y = math.floor(y)
+        return self.data[x][y].set_magnitude(MAX_SPEED)
+
+    def lookup_by_vector(self, position):
+        x = Vector(1, 0) * position
+        y = Vector(0, 1) * position
+        return self.lookup(x, y)
