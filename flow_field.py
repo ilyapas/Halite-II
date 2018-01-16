2b2dce9e45a022cdf49f0b1b691b51388200c8ee
Ilya Pashkovskiy <ilya.pashkovskiy@gmail.com>
2018-01-16 06:29:39 +0100

    added ships consideration for the flow field

--- flow_field.py
+++ flow_field.py
@@ -17,12 +17,14 @@ class FlowField(object):
     def __init__(self, game_map, scale):
         self.game_map = game_map
         self.planet_forces = self._analyze_planets()
+        self.ship_forces = self._analyze_ships()
 
     def lookup(self, x, y):
         return self.lookup_by_vector(Vector(x, y))
 
     def lookup_by_vector(self, position):
         force = self._apply_forces(position, self.planet_forces)
+        force = self._apply_forces(position, self.ship_forces)
         return force
 
     def _apply_forces(self, position, forces):
@@ -31,8 +33,10 @@ class FlowField(object):
             diff = force.origin - position
             distance = diff.norm()
             gauss = force.magnitude * math.exp(-1 * force.kernel * distance)
+            logging.info(f'diff {diff.set_magnitude(gauss)}')
             sum_forces += diff.set_magnitude(gauss)
         sum_forces = sum_forces.set_magnitude(MAX_SPEED - 1)
+        logging.info(f'sum {sum_forces}')
         return sum_forces
 
     def _analyze_planets(self):
@@ -51,7 +55,7 @@ class FlowField(object):
     def _analyze_empty_planets(self, planets):
         forces = []
         for planet in planets:
-            forces.append(Force(Vector(planet.x, planet.y), 0.01, 1000.0))
+            forces.append(Force(Vector(planet.x, planet.y), 0.0001, 1000.0))
         return forces
 
     def _analyze_own_planets(self, planets):
@@ -60,5 +64,28 @@ class FlowField(object):
     def _analyze_enemy_planets(self, planets):
         forces = []
         for planet in planets:
-            forces.append(Force(Vector(planet.x, planet.y), 0.01, 1000.0))
+            forces.append(Force(Vector(planet.x, planet.y), 0.0001, -1000.0))
+        return forces
+
+    def _analyze_ships(self):
+        me = self.game_map.get_me()
+        all_ships = self.game_map._all_ships()
+        own_ships = [p for p in all_ships if p.owner is me]
+        enemy_ships = [p for p in all_ships if p.owner is not me]
+
+        forces = []
+        forces += self._analyze_own_ships(own_ships)
+        forces += self._analyze_enemy_ships(enemy_ships)
+        return forces
+
+    def _analyze_own_ships(self, ships):
+        forces = []
+        for ship in ships:
+            forces.append(Force(Vector(ship.x, ship.y), 0.00001, -100.0))
+        return forces
+
+    def _analyze_enemy_ships(self, ships):
+        forces = []
+        for ship in ships:
+            forces.append(Force(Vector(ship.x, ship.y), 0.0001, 1000.0))
         return forces
