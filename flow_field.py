7fccc775c6f537249ed36f5e8ca1d1fb6fc28fde
Ilya Pashkovskiy <ilya.pashkovskiy@gmail.com>
2018-01-16 06:17:55 +0100

    further progress on the flow field module

--- flow_field.py
+++ flow_field.py
@@ -1,23 +1,64 @@
 import hlt
+import logging
 import math
+import time
 from hlt.constants import MAX_SPEED
 from vector import Vector
 
 
+class Force(object):
+    def __init__(self, origin, kernel, magnitude):
+        self.origin = origin
+        self.kernel = kernel
+        self.magnitude = magnitude
+
+
 class FlowField(object):
-    def __init__(self, game_map):
+    def __init__(self, game_map, scale):
         self.game_map = game_map
-        self.width = game_map.width
-        self.height = game_map.height
-        self.data = [[Vector(1, 0) for y in range(self.height)]
-                     for x in range(self.width)]
+        self.planet_forces = self._analyze_planets()
 
     def lookup(self, x, y):
-        x = math.floor(x)
-        y = math.floor(y)
-        return self.data[x][y].set_magnitude(MAX_SPEED)
+        return self.lookup_by_vector(Vector(x, y))
 
     def lookup_by_vector(self, position):
-        x = Vector(1, 0) * position
-        y = Vector(0, 1) * position
-        return self.lookup(x, y)
+        force = self._apply_forces(position, self.planet_forces)
+        return force
+
+    def _apply_forces(self, position, forces):
+        sum_forces = Vector()
+        for force in forces:
+            diff = force.origin - position
+            distance = diff.norm()
+            gauss = force.magnitude * math.exp(-1 * force.kernel * distance)
+            sum_forces += diff.set_magnitude(gauss)
+        sum_forces = sum_forces.set_magnitude(MAX_SPEED - 1)
+        return sum_forces
+
+    def _analyze_planets(self):
+        me = self.game_map.get_me()
+        all_planets = self.game_map.all_planets()
+        empty_planets = [p for p in all_planets if p.owner is None]
+        own_planets = [p for p in all_planets if p.owner is me]
+        enemy_planets = [p for p in all_planets if p.owner is not me]
+
+        forces = []
+        forces += self._analyze_empty_planets(empty_planets)
+        forces += self._analyze_own_planets(own_planets)
+        forces += self._analyze_enemy_planets(enemy_planets)
+        return forces
+
+    def _analyze_empty_planets(self, planets):
+        forces = []
+        for planet in planets:
+            forces.append(Force(Vector(planet.x, planet.y), 0.01, 1000.0))
+        return forces
+
+    def _analyze_own_planets(self, planets):
+        return list()
+
+    def _analyze_enemy_planets(self, planets):
+        forces = []
+        for planet in planets:
+            forces.append(Force(Vector(planet.x, planet.y), 0.01, 1000.0))
+        return forces
