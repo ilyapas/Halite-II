36d3d4ec69a46da4881c536fbb9db5fde2a5426c
Ilya Pashkovskiy <ilya.pashkovskiy@gmail.com>
2018-01-19 06:28:06 +0100

    added missing file in the release script

--- release.bat
+++ release.bat
@@ -1,3 +1,3 @@
 @echo off
-7z a halite.zip MyBot.py command_center.py flow_field.py hlt
+7z a halite.zip MyBot.py command_center.py vector.py flow_field.py hlt
 .\hlt_client\client.py bot -b halite.zip
